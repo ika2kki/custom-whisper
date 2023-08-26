d4615eb0a6f00a31c9706aba69f75accd0cefe6a
cyrus01337 <cyrus21337@gmail.com>
2023-08-26 03:38:43 +0100

    Format using Prettier

--- override.js
+++ override.js
@@ -1,25 +1,27 @@
-const pattern = /\/w (?<name>.+) Hi! I want to (?<action>buy|sell): (?<item>.+) for (?<cost>[0-9]+) platinum\. \(warframe\.market\)/
+const pattern =
+    /\/w (?<name>.+) Hi! I want to (?<action>buy|sell): (?<item>.+) for (?<cost>[0-9]+) platinum\. \(warframe\.market\)/;
 
-document.addEventListener("copy", async event => {
-  const saved = await browser.storage.local.get("templates")
-  const templates = saved.templates
+document.addEventListener("copy", async (event) => {
+    const saved = await browser.storage.local.get("templates");
+    const templates = saved.templates;
 
-  const contents = document.activeElement?.value
-  if (!contents) {
-    return
-  }
-
-  match = contents.match(pattern)
-  if (match) {
-    event.preventDefault()
-    let template = match.groups.action == "buy" ? templates.buy : templates.sell
-    if (!template) {
-      return
+    const contents = document.activeElement?.value;
+    if (!contents) {
+        return;
     }
-    whisper = `/w {name} ${template}`
-    for (const name in match.groups) {
-      whisper = whisper.replaceAll(`{${name}}`, match.groups[name])
+
+    match = contents.match(pattern);
+    if (match) {
+        event.preventDefault();
+        let template =
+            match.groups.action == "buy" ? templates.buy : templates.sell;
+        if (!template) {
+            return;
+        }
+        whisper = `/w {name} ${template}`;
+        for (const name in match.groups) {
+            whisper = whisper.replaceAll(`{${name}}`, match.groups[name]);
+        }
+        await navigator.clipboard.writeText(whisper);
     }
-    await navigator.clipboard.writeText(whisper)
-  }
-})
+});
--- popup.js
+++ popup.js
@@ -1,22 +1,22 @@
 document.addEventListener("DOMContentLoaded", async function () {
-  const buy = document.getElementById("textbox-buy");
-  const sell = document.getElementById("textbox-sell");
-  const submitButton = document.getElementById("submit");
+    const buy = document.getElementById("textbox-buy");
+    const sell = document.getElementById("textbox-sell");
+    const submitButton = document.getElementById("submit");
 
-  const current = await browser.storage.local.get("templates")
-  const templates = current.templates
+    const current = await browser.storage.local.get("templates");
+    const templates = current.templates;
 
-  if (templates) {
-    buy.value = templates.buy
-    sell.value = templates.sell
-  }
+    if (templates) {
+        buy.value = templates.buy;
+        sell.value = templates.sell;
+    }
 
-  submitButton.addEventListener("click", async function () {
-    await browser.storage.local.set({
-      templates: {
-        buy: buy.value,
-        sell: sell.value
-      }
+    submitButton.addEventListener("click", async function () {
+        await browser.storage.local.set({
+            templates: {
+                buy: buy.value,
+                sell: sell.value,
+            },
+        });
     });
-  });
 });
