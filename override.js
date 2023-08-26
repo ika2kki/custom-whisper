9a82530e943d9754dc3666fa6664aa0d187608c2
cyrus01337 <cyrus21337@gmail.com>
2023-08-26 03:40:08 +0100

    Smol tidying

--- override.js
+++ override.js
@@ -6,22 +6,29 @@ document.addEventListener("copy", async (event) => {
     const templates = saved.templates;
 
     const contents = document.activeElement?.value;
+
     if (!contents) {
         return;
     }
 
     match = contents.match(pattern);
+
     if (match) {
         event.preventDefault();
+
         let template =
             match.groups.action == "buy" ? templates.buy : templates.sell;
+
         if (!template) {
             return;
         }
+
         whisper = `/w {name} ${template}`;
+
         for (const name in match.groups) {
             whisper = whisper.replaceAll(`{${name}}`, match.groups[name]);
         }
+
         await navigator.clipboard.writeText(whisper);
     }
 });
